d9280daae8249854be09e9e988945b7f544dbcb0
amarismariano <mariano32josea@gmail.com>
2022-03-05 11:07:20 -0500

    Conección a base de datos y setting de la misma

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
-const express = require("express");
+import express from "express";
+import connectDB from "./config/db.js";
 
 const app = express();
+
+connectDB();
 
 app.listen(4000, () => {
   console.log("Servidor corriendo en el puesto 4000");
