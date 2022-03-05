3b613e72d4c5b7344320a42dc059724ae39a4a0f
amarismariano <mariano32josea@gmail.com>
2022-03-05 11:21:10 -0500

    Ocultando conexión de la DB en variables d entorno

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(
-      "mongodb+srv://mariano:root@cluster0.lye3f.mongodb.net/uptask?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    const connection = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
     const url = `${connection.connection.host}:${connection.connection.port}`;
     console.log(`MongoDB conectado en: ${url}`);
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 
 const app = express();
 
+dotenv.config();
+
 connectDB();
 
-app.listen(4000, () => {
-  console.log("Servidor corriendo en el puesto 4000");
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
