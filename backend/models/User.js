b750c9e08259127edf68958ee5ad9a0b304ad8f4
amarismariano <mariano32josea@gmail.com>
2022-03-06 11:30:20 -0500

    Implementación de hasheo de pw y middlewares

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = mongoose.Schema(
   {
@@ -30,6 +31,16 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  //Revisa que el password no haya sido cambiado
+  if (!this.isModified("password")) {
+    next(); //Pasa al siguiente middleware
+  }
+  // Ocultamos las contraseñas a la hora del registro
+  const salt = await bcrypt.genSalt(10); // Rondas que hará antes de hashear, entre más rondas mas consumos a la base de datos
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 export default User;
