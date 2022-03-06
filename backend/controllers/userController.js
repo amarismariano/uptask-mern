53b6a458d6968e908ad46a44cba7e2de278b7009
amarismariano <mariano32josea@gmail.com>
2022-03-06 11:20:14 -0500

    Evitar registros duplicados

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,14 @@
 import User from "../models/User.js";
 
 const register = async (req, res) => {
+  //Evitar registros duplicados
+  const { email } = req.body;
+  const userExist = await User.findOne({ email: email });
+  if (userExist) {
+    const error = new Error("Usuario ya registrado");
+    return res.status(400).json({ msg: error.message });
+  }
+
   //Creación de usurios y almacenamiento a la base de datos
   try {
     const user = new User(req.body);
