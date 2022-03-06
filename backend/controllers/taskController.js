0e5192f52a66873eb680cbc5da370425278253f9
amarismariano <mariano32josea@gmail.com>
2022-03-06 16:09:26 -0500

    Creación de rutas de tareas

---
+++ backend/controllers/taskController.js
@@ -0,0 +1,21 @@
+const createTask = async (req, res) => {
+  //Crear tarea
+};
+
+const getTask = async (req, res) => {
+  //Obtener tarea en especifico
+};
+
+const updateTask = async (req, res) => {
+  //Actualizar tarea
+};
+
+const deleteTask = async (req, res) => {
+  //Eliminar tarea
+};
+
+const statusChange = async (req, res) => {
+  //Cambiar estado de la tarea
+};
+
+export { createTask, getTask, updateTask, deleteTask, statusChange };
---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,21 @@
+import express from "express";
+import {
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+  statusChange,
+} from "../controllers/taskController.js";
+import checkAuth from "../middleware/checkAuth.js";
+
+const router = express.Router;
+
+router.post("/", checkAuth, createTask);
+router
+  .route("/:id")
+  .get(checkAuth, getTask)
+  .put(checkAuth, updateTask)
+  .delete(checkAuth, deleteTask);
+router.post("/estado/:id", checkAuth, statusChange);
+
+export default router;
