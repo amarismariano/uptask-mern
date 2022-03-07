3017d822544afec4d97853418e7ab050432c8eaa
amarismariano <mariano32josea@gmail.com>
2022-03-07 17:28:24 -0500

    Auth por correo + seteo de auth de user a true

--- backend/helpers/emails.js
+++ backend/helpers/emails.js
@@ -1,5 +1,32 @@
 import nodemailer from "nodemailer";
 
-export const emailRegister = (data) => {
-  console.log("DATOS", data);
+export const emailRegister = async (data) => {
+  const { email, name, token } = data;
+
+  const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "8a1c7b3db255d2",
+      pass: "768612d1dddc37",
+    },
+  });
+
+  //Información del email
+
+  const info = await transport.sendMail({
+    from: '"UpTask - Administrador de Proyectos" <cuentas@uptask.com>',
+    to: email,
+    subject: "UpTask - Comprueba tu cuenta",
+    text: "Comprueba tu cuenta en UpTask",
+    html: `<p>Hola: ${name} Comprueba tu cuenta en UpTask</p>
+      <p>Tu cuenta ya está casi lista, solo debes comprobarla en el siguiente enlace:
+        <a href="${process.env.FRONTEND_URL}/confirmar/${token}"> Comprobar Cuenta </a>
+
+        <p>Si tu no creaste esta cuenta, puedes ignorar el mensaje</p>
+      </p>
+      
+      
+      `,
+  });
 };
--- frontend/src/pages/ConfirmAccount.jsx
+++ frontend/src/pages/ConfirmAccount.jsx
@@ -1,12 +1,57 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import axios from "axios";
+import Alert from "../components/Alert";
 
 const ConfirmAccount = () => {
+  const [alert, setAlert] = useState({});
+  const [confirmedAccount, setConfirmedAccount] = useState(false);
+
+  const params = useParams();
+  const { id } = params;
+
+  useEffect(() => {
+    const confirmAccount = async () => {
+      try {
+        const url = `http://localhost:4000/api/usuarios/confirmar/${id}`;
+        const { data } = await axios(url);
+
+        setAlert({
+          msg: data.msg,
+          error: false,
+        });
+      } catch (error) {
+        setAlert({
+          msg: error.response.data.msg,
+          error: true,
+        });
+        //Si todo sale bien
+        setConfirmedAccount(true);
+      }
+    };
+    confirmAccount();
+  }, []);
+
+  const { msg } = alert;
+
   return (
     <>
       <h1 className="text-sky-600 fron-black text-6xl capitalize">
         Confirma tu cuenta y comienza a crear tus{" "}
         <span className="text-slate-700">proyectos</span>
       </h1>
+
+      <div className="mt-20 md:mt-10 shadow-lg px-5 py-10 rounded-xl bg-white">
+        {msg && <Alert alert={alert} />}
+        {!confirmedAccount && (
+          <Link
+            className="block text-center my-5 text-slate-500 uppercase text-sm"
+            to="/"
+          >
+            Inicia Sesión!
+          </Link>
+        )}
+      </div>
     </>
   );
 };
