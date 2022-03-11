61d2b946527fe3a30174b49cc3a1ea9f269e0d4a
amarismariano <mariano32josea@gmail.com>
2022-03-11 17:36:57 -0500

    Creando context de autenticación

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,19 +5,22 @@ import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
 import NewPassword from "./pages/NewPassword";
 import ConfirmAccount from "./pages/ConfirmAccount";
+import { AuthProvider } from "./context/AuthProvider";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AuthLayout />}>
-          <Route index element={<Login />} />
-          <Route path="registrar" element={<Register />} />
-          <Route path="olvide-password" element={<ForgotPassword />} />
-          <Route path="olvide-password/:token" element={<NewPassword />} />
-          <Route path="confirmar/:id" element={<ConfirmAccount />} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<AuthLayout />}>
+            <Route index element={<Login />} />
+            <Route path="registrar" element={<Register />} />
+            <Route path="olvide-password" element={<ForgotPassword />} />
+            <Route path="olvide-password/:token" element={<NewPassword />} />
+            <Route path="confirmar/:id" element={<ConfirmAccount />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
